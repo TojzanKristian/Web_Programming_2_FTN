0a829deaa9cf5fe2ed67a81068ecd808e065f55d
Tojzán Krisztián <kristiantojzan@gmail.com>
2024-05-19 14:03:00 +0200

    Models

--- taxi-app-service/Common/Models/MySession.cs
+++ taxi-app-service/Common/Models/MySession.cs
@@ -5,5 +5,6 @@ namespace Common.Models
     public class MySession
     {
         public static Dictionary<string, User> data = new Dictionary<string, User>();
+        public static Dictionary<int, string> waitForTrip = new Dictionary<int, string>();
     }
 }
